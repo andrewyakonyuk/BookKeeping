59c9d6bd0ee0415046e9a2ac0764eb2299f79a5b
andrewyakonyuk <andrewyakonyuk@gmail.com>
2014-05-03 02:46:56 +0300

    remove todos

--- BookKeeping.App/App.xaml.cs
+++ BookKeeping.App/App.xaml.cs
@@ -4,38 +4,6 @@ using System.Windows;
 
 namespace BookKeeping.App
 {
-    //TODO: domain entities: 
-    //  1) store,
-    //  2) warehouse,
-    //  3) product,
-    //  4) order,
-    //  5) vat group,
-    //  6) sku,
-    //  7) incoming
-    //  8) expense
-    //TODO: windows: 
-    //  1) stores list, create new store, edit store
-    //  2) warehouses list, create new warehouse, edit warehouse
-    //  3) 
-    //TODO: commands:
-    //  1) store: 
-    //      a. create store (id, store id, title, warehouses, vat group);
-    //      b. close store (id, store id)
-    //      c. rename (id, store id, new title)
-    //      d. change warehouses(id, store id, warehouses)
-    //      e. change vat group (id, store id, vat group)
-    //  2) warehouses:
-    //      a. create warehouse (id, warehouse id, title)
-    //      b. close warehouse (id, warehouse id)
-    //      c. rename warehouse (id, warehouse id, new title)
-    //      d. purchase (id, product id, quantity, purchase price, markup)
-    //      e. order (id, product id
-    //TODO: extract domain
-    //TODO: implement caching and loging
-    //TODO: create reports (price-list, invoice)
-    //TODO: roles-based user interface
-    //TODO: UI - confirm close edit panel, if product data was changed
-
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
