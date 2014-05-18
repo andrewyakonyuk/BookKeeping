bf53298013709ea23d0a85f664fcd9a7826d6fce
andrewyakonyuk <andrewyakonyuk@gmail.com>
2014-05-18 12:11:21 +0300

    add Supplier and Buyer entities

---
+++ BookKeeping.Domain/Aggregates/Supplier.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BookKeeping.Domain.Aggregates
+{
+    public class Supplier : IEntity
+    {
+        public long Id { get; set; }
+        public Address LegalAddress { get; set; }
+        public string BankingDetails { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+    }
+}
